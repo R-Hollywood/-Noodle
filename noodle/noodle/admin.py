a66947b0bfb882a45c9c58572561520a2ae4c0e2
Ross McBride <mcbrider5002@gmail.com>
2017-03-06 09:12:55 +0000

    Fixed small error in admin.py

--- noodle/noodle/admin.py
+++ noodle/noodle/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from rango.models import Admin, Staff, Student, Course, Subject, Material, File, Assessment
-	
-class PageAdmin(admin.ModelAdmin):
-	list_display = ('category', 'title', 'url')
-	
-class CategoryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Admin)
 admin.site.register(Staff)
