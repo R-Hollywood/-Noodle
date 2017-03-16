ca8c16bce2f1afe1c629584b698c5988bc702e5f
Jack Conacher <jack.conacher@gmail.com>
2017-03-16 22:05:37 +0000

    authentication

--- Noodle_project/urls.py
+++ Noodle_project/urls.py
@@ -2,11 +2,19 @@ from django.conf.urls import url
 from django.contrib import admin
 from noodle import views
 from django.conf.urls import include
+from registration.backends.simple.views import RegistrationView
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,user):
+        return '/noodle/'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^noodle/',include('noodle.urls')),
     url(r'^$',views.home, name='homepage'),
+    url(r'^accounts/register/$',
+        MyRegistrationView.as_view(),
+            name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 
 ]
