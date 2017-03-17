f9e52208c3528c865d33702e0bd0f1b0d667a404
Rachel-Anne Hollywood <2195303H@student.gla.ac.uk>
2017-03-17 14:59:18 +0000

    More template editing

--- noodle/urls.py
+++ noodle/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from noodle import views
 app_name = 'noodle'
-urlpatterns = [
-    url(r'^$',views.home, name='homepage'),
+urlpatternsw\url(r'^$',views.home, name='homepage'),
     url(r'^login/$',views.user_login,name='login'),
     url(r'^logout/$',views.user_logout,name='logout'),
-	url(r'^register/$',views.register,name='register'),
+	url(r'^r
+            gister/$',views.register,name='register'),
     url(r'^register/staff',views.registerStaff,name='registerStaff'),
 	url(r'^register/student',views.registerStudent,name='registerStudent'),
     url(r'teachhome/',views.teachhome, name='teachhome'),
