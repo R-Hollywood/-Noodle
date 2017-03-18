695548a86a91b0d6d375abcc7b7ec5995c0925fc
jack <jack.conacher@gmail.com>
2017-03-18 17:01:43 +0000

    fixed syntax errors

--- noodle/urls.py
+++ noodle/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
 from noodle import views
 app_name = 'noodle'
-urlpatternsw\url(r'^$',views.home, name='homepage'),
+urlpatterns= [ url(r'^$',views.home, name='homepage'),
     url(r'^login/$',views.user_login,name='login'),
     url(r'^logout/$',views.user_logout,name='logout'),
-	url(r'^r
-            gister/$',views.register,name='register'),
+	url(r'^register/$',views.register,name='register'),
     url(r'^register/staff',views.registerStaff,name='registerStaff'),
 	url(r'^register/student',views.registerStudent,name='registerStudent'),
     url(r'teachhome/',views.teachhome, name='teachhome'),
