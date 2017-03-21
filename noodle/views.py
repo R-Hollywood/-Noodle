3fa2e0e7e463efc93b3f4a0005f3e7de75f8ed70
Ivo Domingos <2229639d@student.gla.ac.uk>
2017-03-21 10:39:03 +0000

    views Update

--- noodle/views.py
+++ noodle/views.py
@@ -31,8 +31,24 @@ def render(request, page, context_dict):
 			context_dict['tier'] = 2
 			if(homePage):
 				page = 'noodle/teachhome.html'
-
+	page == 'noodle/homepage_extends_base.html'
 	return shortcuts.render(request, page, context_dict)
+	
+def listingCourse(request):
+	course_list = Course.objects.all()
+	paginator = Paginator(course_list, 25) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	try:
+		contacts = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		course = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		course = paginator.page(paginator.num_pages)
+
+	return render(request, 'list.html', {'course': course})	
 
 def home(request):
 	
