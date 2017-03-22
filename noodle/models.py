def03de854e367cccdd561098cc7ee552443e015
Rachel-Anne Hollywood <2195303H@student.gla.ac.uk>
2017-03-22 10:50:44 +0000

    Fixing merge conflict.Out of Date code.

--- noodle/models.py
+++ noodle/models.py
@@ -8,9 +8,7 @@ class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
     
 class Admin(models.Model):
-	#name, password, email, forename and surname attributes are included with django's 'User' model
-	#in our case we should simply set the username to be the same as the email address
-	#because django has a lot involved in the underlying framework
+	
 	user = models.OneToOneField(User, blank = False, related_name = 'admin')
 	
 	def save(self, *args, **kwargs):
@@ -24,16 +22,13 @@ class Admin(models.Model):
 		return self.user.username
 
 class Staff(models.Model):
-	#'inheritance'
+	
 	user = models.OneToOneField(User, blank = False, related_name = 'staff')
 	
 	subject = models.CharField(max_length = 128)
 	status = models.CharField(max_length = 128)
 	
-	#is this field actually necessary?
-	#I don't think so, but I included it for the time being
-	#staMaintainedBy = models.ManyToManyField(Admin, related_name = 'staMaintenanceOf')
-
+	
 	def __str__(self): 
 		return self.user.username
 		
@@ -57,14 +52,9 @@ class Subject(models.Model):
 class Course(models.Model):
 
 	name = models.CharField(max_length = 128)
-	#I'm assuming we're going to need different urls for each course page here
 	slug = models.SlugField(unique=True)
-	#I'm treating this as another object for ease of implementation
-	#but as in the ER diagram it's practically just an attribute
 	subject = models.ForeignKey(Subject)
-	
 	staffManagers = models.ManyToManyField(Staff, related_name = 'courses')
-	#couMaintainedBy = models.ManyToManyField(Admin, related_name = 'couMaintenanceOf')
 	
 	def save(self, *args, **kwargs):
 		self.slug = slugify(self.name)
@@ -77,14 +67,13 @@ class Course(models.Model):
 		return self.name
 		
 class Student(models.Model):
-	#'inheritance'
+
 	user = models.OneToOneField(User, blank = False, related_name = 'student')
 	
 	subject = models.CharField(max_length = 128)
 	yearOfStudy = models.IntegerField(default = 1)
 	
 	enrolledIn = models.ManyToManyField(Course, related_name = 'students')
-	#stuMaintainedBy = models.ManyToManyField(Admin, related_name = 'stuMaintenanceOf')
 	
 	def __str__(self): 
 		return self.user.username
@@ -96,11 +85,8 @@ class Material(models.Model):
 
 	name = models.CharField(max_length = 128)
 	visibility = models.BooleanField()
-	#I'm assuming we're going to need different urls for each piece of material here
 	slug = models.SlugField(unique=True)
 	
-	#actually, I think this is an implicit relationship from the student accessing the course
-	#accessedBy = models.ManyToManyField(Student, related_name = 'hasAccess')
 	courseFrom = models.ForeignKey(Course, related_name = 'material')
 	createdBy = models.ForeignKey(Staff, related_name = 'createdMaterial', blank = False)
 
@@ -115,7 +101,7 @@ class Material(models.Model):
 		return self.name
 
 class File(models.Model):
-	#'inheritance'
+	
 	material = models.OneToOneField(Material, unique = True)
 	file = models.FileField(blank = True)
 
@@ -126,7 +112,7 @@ class File(models.Model):
 		return self.name
 
 class Assessment(models.Model):
-	#'inheritance'
+	
 	material = models.OneToOneField(Material, unique = True)
 	submission = models.FileField(blank = True)
 	
@@ -141,7 +127,7 @@ class Assessment(models.Model):
 
 
 class UserProfile(models.Model):
-		# Links UserProfile to a User model instance
+		
 	user = models.ImageField(upload_to='profile_images', blank=True)
 
 	def __str__(self):
